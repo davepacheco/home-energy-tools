5aa3647736324b2a2e4a4bcb70c8619c744e4c9f
David Pacheco <dap@oxidecomputer.com>
2021-12-11 11:42:18 -0800

    actually fetch data

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,34 @@
 //! Basic tool for fetching data about solar energy system from Enlighten API
+
+use anyhow::anyhow;
 use anyhow::Context;
+use chrono::TimeZone;
 use openapi::{
     self,
     apis::configuration::{ApiKey, Configuration},
 };
+use std::time::Duration;
 
 /// Describes the config for this tool
 #[derive(serde::Deserialize)]
 struct Config {
     enlighten_key: String,
     enlighten_user_id: String,
+    start_date: chrono::NaiveDate,
 }
 
 /// Wraps `anyhow::Error` in something implementing `std::error::Error`
 #[derive(Debug, thiserror::Error)]
 #[error(transparent)]
 struct AnyhowWrap(#[from] anyhow::Error);
+
+/// Represents an output record
+#[derive(serde::Serialize)]
+struct OutputRecord {
+    // TODO Would it be easier if this were local time?
+    time: chrono::DateTime<chrono::Utc>,
+    energy_wh: i32,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -56,41 +69,56 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     .await
     .context("listing systems")?;
 
+    if response.systems.len() != 1 {
+        // XXX This is absurd.  But how else do we invoke the conversion?
+        // .into() didn't do it.
+        Err(anyhow!(
+            "expected exactly one system, but found {}",
+            response.systems.len()
+        ))?;
+    }
+
     let system_id = response.systems[0].system_id;
-    eprintln!("first system id: {:?}", system_id);
+    let mut date = config.start_date;
+    let last_date = chrono::NaiveDateTime::from_timestamp(
+        chrono::Utc::now().timestamp(),
+        0,
+    )
+    .date();
+    let mut writer = csv::Writer::from_writer(std::io::stdout());
+    while date < last_date {
+        let next_date = date.succ();
+        let stats = enlighten::stats(
+            &enlighten_config,
+            user_id,
+            system_id,
+            Some(date.and_hms(0, 0, 0).timestamp()),
+            Some(next_date.and_hms(0, 0, 0).timestamp()),
+        )
+        .await
+        .with_context(|| format!("fetch stats for {}", date))?;
 
-    // let inventory = enlighten::inventory(&config, user_id, system_id)
-    //     .await
-    //     .context("inventory")?;
-    // eprintln!("{:?}", inventory);
+        for data in stats.intervals {
+            let data_end_time = chrono::Utc.timestamp(data.end_at, 0);
+            let data_start_time = data_end_time
+                .checked_sub_signed(
+                    chrono::Duration::from_std(Duration::from_secs(300))
+                        .unwrap(),
+                )
+                .unwrap(); // XXX
+            writer
+                .serialize(OutputRecord {
+                    time: data_start_time,
+                    energy_wh: data.enwh,
+                })
+                .context("writing record")?;
+        }
 
-    eprintln!(
-        "{:?}",
-        enlighten::stats(&enlighten_config, user_id, system_id, None, None)
-            .await
-            .context("getting stats")?
-    );
+        writer.flush().context("flushing writer")?;
+        date = next_date;
+    }
 
-    // eprintln!(
-    //     "{:?}",
-    //     enlighten::energy_lifetime(
-    //         &config,
-    //         user_id,
-    //         system_id,
-    //         None,
-    //         None,
-    //         Some("all")
-    //     )
-    //     .await
-    //     .context("energy_lifetime")?
-    // );
-
-    // eprintln!(
-    //     "{:?}",
-    //     enlighten::summary(&config, user_id, system_id, None)
-    //         .await
-    //         .context("summary")?
-    // );
+    writer.flush().context("flushing writer")?;
 
     Ok(())
 }
