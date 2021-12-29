618a9ac466747483d1429499955a01a0b4073d9d
David Pacheco <dap@oxidecomputer.com>
2021-12-28 20:44:09 -0800

    pge-munge may as well operate on stdin

--- src/bin/pge-munge.rs
+++ src/bin/pge-munge.rs
@@ -2,29 +2,29 @@
 
 use anyhow::Context;
 use solar_data::pge::ElectricityUsageReader;
-use std::fs::File;
-use std::path::PathBuf;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "pge-munge")]
 #[structopt(no_version)]
-#[structopt(about = "turn raw PG&E data file into something more usable")]
-struct Args {
-    #[structopt(parse(from_os_str), help = "path to raw PG&E file")]
-    input: PathBuf,
-}
+#[structopt(
+    about = "convert PG&E electricity usage data file into a proper CSV \
+    with UTC timestamps"
+)]
+struct Args {}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::from_args();
     Ok(munge_file(&args)?)
 }
 
-fn munge_file(args: &Args) -> Result<(), anyhow::Error> {
-    let file = File::open(&args.input)
-        .with_context(|| format!("open {:?}", args.input.display()))?;
+fn munge_file(_: &Args) -> Result<(), anyhow::Error> {
+    let file = std::io::stdin();
+    if atty::is(atty::Stream::Stdin) {
+        eprintln!("note: reading from stdin");
+    }
     let mut reader = ElectricityUsageReader::new(file)
-        .with_context(|| format!("setup {:?}", args.input.display()))?;
+        .with_context(|| format!("setup reader from stdin"))?;
     let mut csv_writer = csv::Writer::from_writer(std::io::stdout());
     for record in reader.records() {
         csv_writer
