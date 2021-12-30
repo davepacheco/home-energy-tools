f889f53a756f9aae8476938d4666bac93e5a1785
David Pacheco <dap@oxidecomputer.com>
2021-12-29 19:33:53 -0800

    report: discover and load PG&E data

--- src/bin/report.rs
+++ src/bin/report.rs
@@ -4,6 +4,7 @@ use anyhow::bail;
 use anyhow::Context;
 use solar_data::common::SolarProductionReader;
 use solar_data::data_aggregator::{DataAggregator, Source};
+use solar_data::pge::ElectricityUsageReader;
 use std::fs;
 use std::path::Path;
 use structopt::StructOpt;
@@ -23,13 +24,18 @@ fn report(_: &Args) -> Result<(), anyhow::Error> {
     let mut aggr = DataAggregator::new();
     load_production_data(&mut aggr, Path::new("local-data/production"))
         .context("loading solar data")?;
+    load_pge_data(&mut aggr, Path::new("local-data/pge"))
+        .context("loading PG&E data")?;
 
     let nwarnings = aggr.nwarnings;
 
     eprintln!("warnings: {}", aggr.nwarnings);
     eprintln!("production sources: {}", aggr.nprodsources);
     eprintln!("production records: {}", aggr.nprodrecords);
-    eprintln!("production records aggregated: {}", aggr.nproddupsok);
+    eprintln!("production records combined: {}", aggr.nproddupsok);
+    eprintln!("net usage  sources: {}", aggr.nusagesources);
+    eprintln!("net usage  records: {}", aggr.nusagerecords);
+    eprintln!("net usage  records combined: {}", aggr.nusagedupsok);
 
     if nwarnings > 0 {
         bail!(
@@ -60,6 +66,41 @@ fn load_production_data(
         aggr.load_production(
             Source::new(&filepath.display().to_string()),
             solar_reader.records(),
+        )
+        .with_context(|| format!("loading data from {:?}", filepath))?;
+    }
+
+    Ok(())
+}
+
+// TODO-cleanup commonize with load_production_data
+fn load_pge_data(
+    aggr: &mut DataAggregator,
+    path: &Path,
+) -> Result<(), anyhow::Error> {
+    eprintln!("loading PG&E data from {:?}", path);
+    let dirents = fs::read_dir(path)
+        .with_context(|| format!("readdir {:?}", path.display()))?;
+    for maybe_item in dirents {
+        let item = maybe_item
+            .with_context(|| format!("readdir {:?} entry", path.display()))?;
+        let filepath = item.path();
+        let name = item.file_name();
+        let name_str = name.to_string_lossy();
+        if !name_str.starts_with("pge_electric_interval_data_")
+            || !name_str.ends_with(".csv")
+        {
+            eprintln!("skipping {:?}", filepath);
+            continue;
+        }
+        eprintln!("loading PG&E data from {:?}", filepath);
+        let file = fs::File::open(&filepath)
+            .with_context(|| format!("read {:?}", filepath))?;
+        let mut data_reader = ElectricityUsageReader::new(file)
+            .with_context(|| format!("load initial {:?}", filepath))?;
+        aggr.load_net_usage(
+            Source::new(&filepath.display().to_string()),
+            data_reader.records(),
         )
         .with_context(|| format!("loading data from {:?}", filepath))?;
     }
--- src/data_aggregator.rs
+++ src/data_aggregator.rs
@@ -2,6 +2,7 @@
 // TODO There's probably a crate well-suited for this.
 
 use crate::common::EnergyProduced;
+use crate::common::NetEnergyUsed;
 use crate::common::WattHours;
 use anyhow::ensure;
 use chrono::Timelike;
@@ -26,6 +27,9 @@ pub struct DataAggregator {
     pub nproddupsok: usize,
     pub nprodsources: usize,
     pub nprodrecords: usize,
+    pub nusagedupsok: usize,
+    pub nusagesources: usize,
+    pub nusagerecords: usize,
 }
 
 // TODO This feels awfully wasteful when the vast majority of the time these
@@ -45,6 +49,9 @@ impl DataAggregator {
             nproddupsok: 0,
             nprodsources: 0,
             nprodrecords: 0,
+            nusagedupsok: 0,
+            nusagesources: 0,
+            nusagerecords: 0,
         }
     }
 
@@ -105,6 +112,69 @@ impl DataAggregator {
         }
 
         self.nprodrecords += 1;
+
+        Ok(())
+    }
+
+    // TODO-cleanup commonize with load_production
+    pub fn load_net_usage<I>(
+        &mut self,
+        source: Source,
+        iter: I,
+    ) -> Result<(), anyhow::Error>
+    where
+        I: Iterator<Item = Result<NetEnergyUsed, anyhow::Error>>,
+    {
+        let source = Rc::new(source);
+        ensure!(
+            !self.sources.contains(&source),
+            "attempted to load duplicate source: {:?}",
+            source
+        );
+        self.sources.insert(source.clone());
+
+        self.nusagesources += 1;
+        for record in iter {
+            let result =
+                record.and_then(|r| self.load_net_usage_record(&source, r));
+            if let Err(error) = result {
+                eprintln!("warn: {:#}", error);
+                self.nwarnings += 1;
+            }
+        }
+
+        Ok(())
+    }
+
+    // TODO-cleanup commonize with load_production_record
+    fn load_net_usage_record(
+        &mut self,
+        source: &Rc<Source>,
+        energy_used: NetEnergyUsed,
+    ) -> Result<(), anyhow::Error> {
+        let start = &energy_used.timestamp_start_utc;
+        let key_timestamp = start
+            .with_minute(0)
+            .unwrap()
+            .with_second(0)
+            .unwrap()
+            .with_nanosecond(0)
+            .unwrap();
+        let hourly =
+            self.hourly_data.entry(key_timestamp).or_insert_with(|| {
+                HourlyData {
+                    production: BTreeMap::new(),
+                    net_usage: BTreeMap::new(),
+                }
+            });
+        if let Some(old_wh) = hourly.net_usage.get_mut(source) {
+            self.nusagedupsok += 1;
+            *old_wh += energy_used.net_used_wh;
+        } else {
+            hourly.net_usage.insert(source.clone(), energy_used.net_used_wh);
+        }
+
+        self.nusagerecords += 1;
 
         Ok(())
     }
